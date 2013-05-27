35df6a3d5fddc94beb30da4d86cc601ffc02e54d
Jayml <jayml.mistry@gmail.com>
2013-05-27 09:32:20 -0700

    word analytics basically done

--- word-analytics[easy].rb
+++ word-analytics[easy].rb
@@ -11,52 +11,11 @@
 require 'pry'
 require 'pry-debugger'
 
-class RadFile < File
+class RadFile
 
-  def contents
-    self.read
+  def initialize(file)
+    @contents = File.new(file).read
   end
-
-  def contents_array #returns with no symbols
-    workarray = contents.split(" ")
-    workarray.each do |i|
-      i.gsub(/[^a-zA-Z0-9]/, "")
-    end
-    workarray.delete("")
-  end
-
-  def symbols_array
-    workarray = contents.split(" ")
-    workarray.each do |i|
-      i.gsub(/[a-zA-Z0-9]/, "")
-    end
-    workarray.delete("")
-  end
-
-  def contents_joined
-    contents_array.join
-  end
-
-  def hasher(array)
-    ordinator = {}
-    clean_array = array.map do |item|
-      item.gsub(/[^a-zA-Z0-9]/, "").downcase
-    end
-
-    clean_array.delete("")
-    clean_array.map! { |item| item.to_sym }
-
-    clean_array.each do |item|
-      if ordinator[item]
-        ordinator[item] += 1
-      else
-        ordinator[item] = 1
-      end
-    end
-    ordinator.sort_by{ |k,v| v }.reverse
-  end
-
-#---------------------
 
   def wordcount
     contents_array.length
@@ -71,16 +30,100 @@ class RadFile < File
   end
 
   def common_words
-    hasher(contents_array)[0,3]
+    hasher(contents_array).sort_by{ |k,v| v }.reverse[0,3]
+  end
+
+  def single_use_words
+    counter = 0
+    hasher(contents_array).each do | k, v |
+      if v == 1
+        counter += 1
+      end
+    end
+    counter
+  end
+
+  def paragraph_firsts
+    firsts = []
+    paragraph_array.each do |array|
+      firsts << array.first
+    end
+    firsts.join("\n")
   end
 
   def common_letters
-    hasher(contents_joined.split(""))[0,3]
+    hasher(contents_joined.split("")).sort_by{ |k,v| v }.reverse[0,3]
   end
 
+  def all_letters
+    letters = []
+    hasher(contents_joined.split("")).each do | k, v |
+      letters << k
+    end
+    letters.sort.join(" ")
+  end
+
+  #--------------------
+
+  def contents
+    @contents
+  end
+
+  def paragraph_array
+    paragraph = array_maker(/[^a-zA-Z0-9]/, "\n")
+    paragraph.map! do |string|
+      string.split(" ")
+    end
+    paragraph
+  end
+
+  def contents_array #returns with no symbols
+    array_maker(/[^a-zA-Z0-9]/, " ")
+  end
+
+  def symbols_array #returns only symbols
+    array_maker(/[a-zA-Z0-9]/, " ")
+  end
+
+  def contents_joined
+    contents_array.join
+  end
+
+  def array_maker(regexp, split) #takes a regexp to clean the array
+    text = self.contents.split(split)
+    text.each do |i|
+      i.gsub(regexp, "")
+    end
+    text.delete("")
+    text
+  end
+
+  def hasher(array) # returns a hash of counted items
+    ordinator = {}
+    clean_array = array.map do |item|
+      item.gsub(/[^a-zA-Z0-9]/, "").downcase
+    end
+
+    clean_array.delete("")
+    clean_array.map! { |item| item }
+
+    clean_array.each do |item|
+      if ordinator[item]
+        ordinator[item] += 1
+      else
+        ordinator[item] = 1
+      end
+    end
+    ordinator
+  end
 end
 
-
 input = RadFile.new("ascii-text.txt")
-
-puts input.common_letters
+puts "#{input.wordcount} words
+#{input.lettercount} letters
+#{input.symbolcount} symbols
+Top three most common words: #{input.common_words}
+Top three most common letters: #{input.common_letters}
+First words of each paragraph: #{input.paragraph_firsts}
+Number of words only used once: #{input.single_use_words}
+Letters used: #{input.all_letters}"
